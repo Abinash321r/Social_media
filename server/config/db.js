0c787e9073ae5d3889b8b042a99e84683e6ca9ef
Abinash321r <121849914+Abinash321r@users.noreply.github.com>
2026-06-26 22:37:48 +0545

    Enhance MongoDB connection with pooling options
    Added connection options for MongoDB to manage socket connections and timeouts.

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,12 @@ dns.setServers(["8.8.8.8", "8.8.4.4", ...dns.getServers()]);
 const connectDB = async () => {
   try {
     //,{ family: 4 }
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+  maxPoolSize: 10, // Maintain up to 10 socket connections (default is 100)
+  minPoolSize: 2,  // Maintain a minimum of 2 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+});
     console.log("MongoDB Atlas Connected");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
