a35bdc45a8b641701e8f09d87560202584cba503
Abinash321r <121849914+Abinash321r@users.noreply.github.com>
2026-06-26 23:10:01 +0545

    Change minPoolSize to maxPoolSize in DB connection

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,7 @@ dns.setServers(["8.8.8.8", "8.8.4.4", ...dns.getServers()]);
 const connectDB = async () => {
   try {
     //,{ family: 4 }
-    await mongoose.connect(process.env.MONGO_URI,{minPoolSize: 300});
+    await mongoose.connect(process.env.MONGO_URI,{maxPoolSize: 300});
     console.log("MongoDB Atlas Connected");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
